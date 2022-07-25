0eb98d98b37aba769430b347b55a8924a615fd78
flt3150sk <muranaka.kouske@gmail.com>
2022-07-25 21:03:33 +0900

    fix nav

--- src/components/features/Nav/index.tsx
+++ src/components/features/Nav/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { useCallback, useState } from 'react'
 import { BsFillChatFill } from 'react-icons/bs'
 import { FaHome, FaUserAlt } from 'react-icons/fa'
@@ -7,59 +8,23 @@ import { NavItem } from './NavItem'
 import { NavItemContainer } from './NavItemContainer'
 import { styles } from './index.styles'
 
-type NavTypes = 'Home' | 'Profile' | 'Messages' | 'Setting' | 'Help' | 'Password' | 'SignOut'
 type Props = {}
 
 export const Nav: React.FC<Props> = () => {
-  const [nav, setNav] = useState<NavTypes>('Home')
+  const { pathname } = useRouter()
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const onClickMenu = useCallback(() => setIsMenuOpen((prev) => !prev), [])
 
   return (
     <nav css={[styles.navigation, isMenuOpen ? styles.wide : styles.narrow]}>
       <NavItemContainer>
-        <NavItem
-          title='Home'
-          icon={FaHome}
-          path={'/'}
-          isActive={nav === 'Home'}
-        />
-        <NavItem
-          title='Profile'
-          icon={FaUserAlt}
-          path={'/profile'}
-          isActive={nav === 'Profile'}
-        />
-        <NavItem
-          title='Messages'
-          icon={BsFillChatFill}
-          path={'/'}
-          isActive={nav === 'Messages'}
-        />
-        <NavItem
-          title='Setting'
-          icon={IoSettingsSharp}
-          path={'/'}
-          isActive={nav === 'Setting'}
-        />
-        <NavItem
-          title='Help'
-          icon={MdOutlineHelp}
-          path={'/'}
-          isActive={nav === 'Help'}
-        />
-        <NavItem
-          title='Password'
-          icon={MdLockOutline}
-          path={'/'}
-          isActive={nav === 'Password'}
-        />
-        <NavItem
-          title='SignOut'
-          icon={MdLogout}
-          path={'/'}
-          isActive={nav === 'SignOut'}
-        />
+        <NavItem title='Home' icon={FaHome} path={'/'} isActive={pathname === '/'} />
+        <NavItem title='Profile' icon={FaUserAlt} path={'/profile'} isActive={pathname === '/profile'} />
+        <NavItem title='Messages' icon={BsFillChatFill} path={'/'} isActive={pathname === 'Messages'} />
+        <NavItem title='Setting' icon={IoSettingsSharp} path={'/'} isActive={pathname === 'Setting'} />
+        <NavItem title='Help' icon={MdOutlineHelp} path={'/'} isActive={pathname === 'Help'} />
+        <NavItem title='Password' icon={MdLockOutline} path={'/'} isActive={pathname === 'Password'} />
+        <NavItem title='SignOut' icon={MdLogout} path={'/'} isActive={pathname === 'SignOut'} />
       </NavItemContainer>
       <div css={styles.menuContainer}>
         <button css={styles.menu} onClick={onClickMenu}>
